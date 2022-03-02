e2946e128176412e0d1f4dbf2b653cfd4d2d632f
Haichao Li <Haichao.li@arm.com>
2022-03-02 14:17:26 +0800

    modify

--- 12/0053.Maximum-Subarray(AC)/53. Maximum Subarray.go
+++ 12/0053.Maximum-Subarray(AC)/53. Maximum Subarray.go
@@ -5,19 +5,18 @@ func maxSubArray(nums []int) int {
 	if len(nums) == 0 {
 		return 0
 	}
-	if len(nums) == 1 {
-		return nums[0]
-	}
+
 	dp, res := make([]int, len(nums)), nums[0]
 	dp[0] = nums[0]
 	for i := 1; i < len(nums); i++ {
 		if dp[i-1] > 0 {
-			dp[i] = nums[i] + dp[i-1]
+			dp[i] = dp[i-1] + nums[i]
 		} else {
 			dp[i] = nums[i]
 		}
 		res = max(res, dp[i])
 	}
+
 	return res
 }
 
